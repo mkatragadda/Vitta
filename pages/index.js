6f95c1ae9b4b9325bc7dcef592291028217b166b
“mkatragadda” <marxk20@gmail.com>
2025-08-05 01:36:07 -0700

    Updating browser title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Vitta - AI Document Chat</title>
+        <title>Vitta - Family finance assistant with AI</title>
         <meta name="description" content="Chat with your financial documents using AI" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
